a3339e721004b1473d8240ca7dadee6953e76d04
AlexKir008 <kirshaav@yandex.ru>
2021-09-27 19:43:17 +0300

    0. Что такое DI? [#6876 #209200]

---
+++ src/main/java/di/Main.java
@@ -0,0 +1,21 @@
+package di;
+
+public class Main {
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.reg(Store.class);
+        context.reg(StartUI.class);
+        context.reg(ConsoleInput.class);
+
+        StartUI ui = context.get(StartUI.class);
+        ConsoleInput cl = context.get(ConsoleInput.class);
+
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+
+        String answer = cl.askStr("Write something ...");
+        ui.add(answer);
+        ui.print();
+    }
+}
